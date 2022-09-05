89633c283cc40961649d3b0bc588a13ec0c231ef
Caleb-Campbell <calebcampbellcrm@gmail.com>
2022-09-05 12:53:34 -0600

    Finished MVP will come back to stretch for practice.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
+  const [data, setData] = useState([]) 
+
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(()=> {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => setData(res.data))
+    .catch(err => console.error(err))
+  }, [])
+
+
+  const StyledCharBox = styled.div`
+  
+  background-color: rgb(0, 0, 0, .7);
+  width: 30%;
+  margin: 0 auto;
+  padding: 1%;
+  border-radius: 1em;
+
+  h3 {
+    padding: 1%;
+    padding-top: 4%;
+    padding-bottom: 4%;
+    color: white;
+  }
+
+  
+  `
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <StyledCharBox>
+      <Character data={data}/>
+      </StyledCharBox>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,15 @@
 // Write your Character component here
+import React from 'react'
+
+
+function Character(props) {
+  return (
+    <div>{
+        props.data.map(char => {
+            return <h3>{char.name}</h3>
+        })
+        }</div>
+  )
+}
+
+export default Character
